b467f19460e40bae075e383d85e57928963d9051
ramibotros <ramibotros@gmail.com>
2016-09-05 15:02:15 -0700

    Removed Comment

--- model/graph.py
+++ model/graph.py
@@ -8,15 +8,6 @@ data_input = tf.placeholder(tf.float32, [model_specs.BATCH_SIZE, data_specs.WIND
 data_targets = tf.placeholder(tf.float32, [model_specs.BATCH_SIZE, data_specs.WINDOW_SIZE])
 
 
-# class MyGridCell(GridRNNCell):
-#   """1D BasicLSTM cell"""
-#   def __init__(self, num_units, forget_bias=1):
-#     super(MyGridCell, self).__init__(num_units=num_units, num_dims=2,
-#                                              input_dims=data_specs.AMINO_PARAM_SIZE, output_dims=0, priority_dims=0, tied=False,
-#                                              cell_fn=lambda n, i: tf.nn.rnn_cell.BasicLSTMCell(num_units=n,
-#                                                                                 forget_bias=forget_bias, input_size=i))
-
-
 lstm_cell = Grid2BasicLSTMCell(model_specs.HIDDEN_LAYER_SIZE)
 multi_lstm_cell = tf.nn.rnn_cell.MultiRNNCell([lstm_cell] * model_specs.NUM_HIDDEN_LAYERS)
 
